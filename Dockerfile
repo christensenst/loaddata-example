80a49e4ad1af1a501ae6718a5052ed54dec3ce9f
Stephen Christensen <schristensen@mindmixer.com>
2018-01-02 14:49:30 -0600

    add test that fails due to loaddata issue

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,8 @@
 FROM python:3.6.4-stretch
+
+ENV WORKDIR="/usr/src/app/"
+RUN mkdir -p "${WORKDIR}"
+WORKDIR "${WORKDIR}"
+COPY requirements.txt "${WORKDIR}"
+RUN pip3 install -r requirements.txt --no-deps
+COPY loaddata_example "${WORKDIR}"
--- loaddata_example/loaddata_bug/app_two/tests.py
+++ loaddata_example/loaddata_bug/app_two/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from app_one.models import Office, Person
+from app_two.models import Author, Book
+
+
+class TestAppTwo(TestCase):
+    fixtures = ['app_one_data', 'app_two_data']
+    multi_db = True
+
+    def test_lengths(self):
+        self.assertEquals(len(Book.objects.all()), 2)
+        self.assertEquals(len(Author.objects.all()), 2)
+        self.assertEquals(len(Office.objects.all()), 2)
+        self.assertEquals(len(Person.objects.all()), 2)
